e1aa85fa824d83b08154c90967775b228f6c4d35
fire8327 <fire83274@gmail.com>
2023-04-11 18:13:22 +0300

    create index

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -4,20 +4,22 @@ $("#toggler").click(()=>{
   $("#toggler img:first-child").toggleClass("hidden");
   $("#toggler img:last-child").toggleClass("hidden");
   $("#menu").toggleClass("-translate-y-[calc(100%+43px)]");
-  $("header").toggleClass("border-b");
-  $("header").toggleClass("pb-px");
+  $("#nav").toggleClass("border-b");
+  $("#nav").toggleClass("pb-px");
 });
 
 
 
 /* dropdown */
-let list = $('.dropdown-list');
-let img = $('.dropdown-img');
-let dropdown = $('.dropdown-toggler');
-dropdown.click(function(e) {
-  e.preventDefault();
-  list.slideToggle(500);
-  img.toggleClass("rotate-180");
-  dropdown.toggleClass("border-b");
-  dropdown.toggleClass("pb-0");
+
+$(".dropdown").each((i,el) => { 
+  let list =  $(el).find('.dropdown-list');
+  let img = $(el).find('.dropdown-img');
+  let dropdown = $(el).find('.dropdown-toggler');
+  dropdown.click(() => {
+    list.slideToggle(500);
+    img.toggleClass("rotate-180");
+    dropdown.toggleClass("border-b");
+    dropdown.toggleClass("pb-2");
+  });
 });
