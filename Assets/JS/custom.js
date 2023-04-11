94f31a9d7384710512d31f016554011a1ce31e54
fire8327 <fire83274@gmail.com>
2023-04-11 14:29:36 +0300

    create header&menu

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,25 +1,21 @@
-/* navigation */
 /*jshint esnext: true */
-const toggler = document.getElementById("toggler");
-const mNav = document.getElementById("mobile__nav");
-const close = document.getElementById("toggler__expanded");
-const overlay = document.getElementById("overlay");
-const body = document.getElementsByTagName("body")[0];
-
-toggler.addEventListener("click", () => {
-  mNav.classList.remove("translate-x-full");
-  overlay.classList.remove("hidden");
-  body.classList.add("overflow-hidden");
+/* navigation */
+$("#toggler").click(()=>{
+  $("#toggler img:first-child").toggleClass("hidden");
+  $("#toggler img:last-child").toggleClass("hidden");
+  $("#menu").toggleClass("-translate-y-[calc(100%+43px)]");
 });
 
-close.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden");
-  body.classList.remove("overflow-hidden"); 
-});
 
-overlay.addEventListener("click", () => {
-  mNav.classList.add("translate-x-full");
-  overlay.classList.add("hidden"); 
-  body.classList.remove("overflow-hidden"); 
+
+/* dropdown */
+let list = $('.dropdown-list');
+let img = $('.dropdown-img');
+let dropdown = $('.dropdown-toggler');
+dropdown.click(function(e) {
+  e.preventDefault();
+  list.slideToggle(500);
+  img.toggleClass("rotate-180");
+  dropdown.toggleClass("border-b");
+  dropdown.toggleClass("pb-2");
 });
